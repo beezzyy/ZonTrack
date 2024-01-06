268ee639213c07af6ea0e342872ca039796d6e1f
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2024-01-05 21:08:42 -0500

    remove max duration

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import Product from '@/lib/models/product.model';
 import { scrapeAmazonProduct } from '@/lib/scraper';
 import { generateEmailBody, sendEmail } from '@/lib/nodemailer';
 
-// export const maxDuration = 300;
+// export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
@@ -23,7 +23,7 @@ export async function GET(request: Request) {
 
     if (!products) throw new Error('No product fetched');
 
-    //1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
+    // ======================== 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
     const updatedProducts = await Promise.all(
       products.map(async (currentProduct) => {
         // Scrape product
@@ -54,7 +54,7 @@ export async function GET(request: Request) {
           product
         );
 
-        //2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
+        // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
         const emailNotifType = getEmailNotifType(
           scrapedProduct,
           currentProduct
